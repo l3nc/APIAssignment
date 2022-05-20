2acd38e4967ce40dc3bb05103d3672897011d401
Nelson <faijaisalui@gmail.com>
2022-05-21 01:22:58 +0800

    error handling update!

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 //const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // Connect to mongoDB server
 dotenv.config({ path: './config.env' });
-
 const uri =
   'mongodb+srv://nelson:kyneMIuqlsfNP6pq@cluster0.l7nav.mongodb.net/assignments?retryWrites=true&w=majority';
 
@@ -24,5 +30,20 @@ const app = require('./app');
 
 const port = 6000;
 app.listen(port, () => {
-  console.log(`Dog App is running on port ${port}`);
+  console.log(`Dog App is running on port ${port}🎉🎊🥂🎉🎊🥂🎉🎊🥂`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...!!!!!');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
