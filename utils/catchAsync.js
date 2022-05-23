51c846afec47ddb6921c5afda77c1e1e12dda0d1
Nelson <faijaisalui@gmail.com>
2022-05-23 18:54:23 +0800

    with JSDoc

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,8 +1,8 @@
 /**
-// help catch any error if it happens
- * 
- * @param {*} fn 
- * @returns 
+ * help catch any error if it happens
+ *
+ * @param {*} fn
+ *
  */
 module.exports = (fn) => {
   return (req, res, next) => {
